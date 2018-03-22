f8f5d0af11620dcb51cef98ef7b1c46cf3e82eab
Dietze <Dietze@NBC0404413.consinto.com>
2018-03-22 17:29:58 +0100

    new Project

---
+++ StundenKalkulation/src/main/java/de/eric/StundenKalkulationApplication.java
@@ -0,0 +1,12 @@
+package de.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StundenKalkulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StundenKalkulationApplication.class, args);
+	}
+}
