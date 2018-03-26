f015335595cc5386f40dddb7e16723e7abe0dc8d
Dietze <Dietze@NBC0404413.consinto.com>
2018-03-26 11:11:55 +0200

    runnable

---
+++ StundenKalkulation/src/main/java/de/eric/UI/LoginUI.java
@@ -0,0 +1,15 @@
+package de.eric.UI;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class LoginUI extends UI{
+
+	@Override
+	protected void init(VaadinRequest request) {
+						
+	}
+
+}
