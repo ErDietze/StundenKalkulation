82d6a5145b0d62051778b9d460f8982b819477c5
Dietze <Dietze@NBC0404413.consinto.com>
2018-03-26 15:53:11 +0200

    EnterButton

--- StundenKalkulation/src/main/java/de/eric/UI/LoginUI.java
+++ StundenKalkulation/src/main/java/de/eric/UI/LoginUI.java
@@ -1,43 +1,53 @@
 package de.eric.UI;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 @SpringUI
-public class LoginUI extends UI{
+public class LoginUI extends UI {
 
 	private VerticalLayout loginRoot;
-	
+
 	@Override
 	protected void init(VaadinRequest request) {
-		
+
 		setupLoginRoot();
 		addHeader();
 		addForm();
-		addLoginButton();	
-		
+		addLoginButton();
+
 	}
 
 	private void setupLoginRoot() {
-		// TODO Auto-generated method stub
-		
+		loginRoot = new VerticalLayout();
+		setContent(loginRoot);
+
 	}
 
 	private void addHeader() {
-		// TODO Auto-generated method stub
-		
+		loginRoot.addComponent(new Label("Stundenkalkulator"));
 	}
 
 	private void addForm() {
-		// TODO Auto-generated method stub
+		HorizontalLayout innerLayout = new HorizontalLayout();
 		
+		
+
 	}
 
 	private void addLoginButton() {
-		// TODO Auto-generated method stub
-		
+		Button loginButton = new Button();
+		loginButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		loginButton.setIcon(VaadinIcons.ENTER);
+		loginRoot.addComponent(loginButton);
+
 	}
 
 }
