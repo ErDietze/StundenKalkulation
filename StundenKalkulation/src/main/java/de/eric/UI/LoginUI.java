64dded984f64ccd89b2fc4a02eb71cd1033ab1cb
Dietze <Dietze@NBC0404413.consinto.com>
2018-03-26 15:19:04 +0200

    addForm

--- StundenKalkulation/src/main/java/de/eric/UI/LoginUI.java
+++ StundenKalkulation/src/main/java/de/eric/UI/LoginUI.java
@@ -3,13 +3,41 @@ package de.eric.UI;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
 public class LoginUI extends UI{
 
+	private VerticalLayout loginRoot;
+	
 	@Override
 	protected void init(VaadinRequest request) {
-						
+		
+		setupLoginRoot();
+		addHeader();
+		addForm();
+		addLoginButton();	
+		
+	}
+
+	private void setupLoginRoot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addHeader() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addForm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addLoginButton() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
